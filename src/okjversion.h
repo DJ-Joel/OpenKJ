3b155a74bb7bf9b0c7bfd5b243d1e92ebb9cab3d
DJ-Joel <joel.hemphill@gmail.com>
2026-07-31 08:06:22 -0500

    Change version branch from 'unstable' to 'stable'

--- src/okjversion.h
+++ src/okjversion.h
@@ -5,6 +5,6 @@
 #define OKJ_VERSION_MINOR 1
 #define OKJ_VERSION_BUILD 41
 #define OKJ_VERSION_STRING "2.1.41"
-#define OKJ_VERSION_BRANCH "unstable"
+#define OKJ_VERSION_BRANCH "stable"
 
 #endif //OKJVERSION_H
